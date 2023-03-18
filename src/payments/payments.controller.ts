475bae6812671bac3b97132d16ced115581e1364
김승녕 <sn@gimseungnyeong-ui-MacBookPro.local>
2023-03-18 17:12:12 +0900

    payment controller add

---
+++ src/payments/payments.controller.ts
@@ -0,0 +1,11 @@
+import { Body, Controller, Post } from "@nestjs/common";
+
+@Controller("payments")
+export class PaymentsController {
+  @Post()
+  processPaddlePayment(@Body() body) {
+    return {
+      ok: true,
+    };
+  }
+}
